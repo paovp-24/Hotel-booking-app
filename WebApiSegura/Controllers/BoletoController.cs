f64ef6a295f3066a5be93b41cb7b74fc598ceb14
Royvargas55 <royvargas55@gmail.com>
2021-03-04 21:06:41 -0600

    adding controllers vuelo & boleto

---
+++ WebApiSegura/Controllers/BoletoController.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSegura.Models;
+
+namespace WebApiSegura.Controllers
+{
+    //[Authorize]
+    [RoutePrefix("api/boleto")]
+    public class BoletoController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetId(int id)
+        {
+            Boleto boleto = new Boleto();
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT BOL_ID
+                                                                    ,USU_CODIGO
+                                                                    ,VUE_ID
+                                                                    ,PAGO_ID
+                                                                    ,BOL_FEC_IDA
+                                                                    ,BOL_FEC_VUELTA
+                                                                    ,BOL_PRECIO
+                                                                    ,BOL_ASIENTO
+                                                                    ,BOL_TERMINAL FROM BOLETO
+                                                            WHERE BOL_ID = @BOL_ID", sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@BOL_ID", id);
+
+                    sqlConnection.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        boleto.BOL_ID = sqlDataReader.GetInt32(0);
+                        boleto.USU_CODIGO = sqlDataReader.GetInt32(1);
+                        boleto.VUE_ID = sqlDataReader.GetInt32(2);
+                        boleto.PAGO_ID = sqlDataReader.GetInt32(3);
+                        boleto.BOL_FEC_IDA = sqlDataReader.GetDateTime(4);
+                        boleto.BOL_FEC_VUELTA = sqlDataReader.GetDateTime(5);
+                        boleto.BOL_PRECIO = sqlDataReader.GetDecimal(6);
+                        boleto.BOL_ASIENTO = sqlDataReader.GetString(7);
+                        boleto.BOL_TERMINAL = sqlDataReader.GetString(8);
+                    }
+
+                    sqlConnection.Close();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Ok(boleto);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            List<Boleto> boletos = new List<Boleto>();
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT BOL_ID
+                                                                    ,USU_CODIGO
+                                                                    ,VUE_ID
+                                                                    ,PAGO_ID
+                                                                    ,BOL_FEC_IDA
+                                                                    ,BOL_FEC_VUELTA
+                                                                    ,BOL_PRECIO
+                                                                    ,BOL_ASIENTO
+                                                                    ,BOL_TERMINAL FROM BOLETO", sqlConnection);
+
+                    sqlConnection.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Boleto boleto = new Boleto()
+                        {
+                            BOL_ID = sqlDataReader.GetInt32(0),
+                            USU_CODIGO = sqlDataReader.GetInt32(1),
+                            VUE_ID = sqlDataReader.GetInt32(2),
+                            PAGO_ID = sqlDataReader.GetInt32(3),
+                            BOL_FEC_IDA = sqlDataReader.GetDateTime(4),
+                            BOL_FEC_VUELTA = sqlDataReader.GetDateTime(5),
+                            BOL_PRECIO = sqlDataReader.GetDecimal(6),
+                            BOL_ASIENTO = sqlDataReader.GetString(7),
+                            BOL_TERMINAL = sqlDataReader.GetString(8)
+                        };
+
+                        boletos.Add(boleto);
+                    }
+
+
+                    sqlConnection.Close();
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(boletos);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Ingresar(Boleto boleto)
+        {
+            if (boleto == null)
+                return BadRequest();
+
+            if (RegistrarBoleto(boleto))
+                return Ok(boleto);
+            else
+                return InternalServerError();
+        }
+
+        private bool RegistrarBoleto(Boleto boleto)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO BOLETO (USU_CODIGO
+                                                                            ,VUE_ID
+                                                                            ,PAGO_ID
+                                                                            ,BOL_FEC_IDA
+                                                                            ,BOL_FEC_VUELTA
+                                                                            ,BOL_PRECIO
+                                                                            ,BOL_ASIENTO
+                                                                            ,BOL_TERMINAL) 
+                                                            VALUES (@USU_CODIGO
+                                                                    ,@VUE_ID
+                                                                    ,@PAGO_ID
+                                                                    ,@BOL_FEC_IDA
+                                                                    ,@BOL_FEC_VUELTA
+                                                                    ,@BOL_PRECIO
+                                                                    ,@BOL_ASIENTO
+                                                                    ,@BOL_TERMINAL)", sqlConnection);
+
+                sqlCommand.Parameters.AddWithValue("@USU_CODIGO", boleto.USU_CODIGO);
+                sqlCommand.Parameters.AddWithValue("@VUE_ID", boleto.VUE_ID);
+                sqlCommand.Parameters.AddWithValue("@PAGO_ID", boleto.PAGO_ID);
+                sqlCommand.Parameters.AddWithValue("@BOL_FEC_IDA", boleto.BOL_FEC_IDA);
+                sqlCommand.Parameters.AddWithValue("@BOL_FEC_VUELTA", boleto.BOL_FEC_VUELTA);
+                sqlCommand.Parameters.AddWithValue("@BOL_PRECIO", boleto.BOL_PRECIO);
+                sqlCommand.Parameters.AddWithValue("@BOL_ASIENTO", boleto.BOL_ASIENTO);
+                sqlCommand.Parameters.AddWithValue("@BOL_TERMINAL", boleto.BOL_TERMINAL);
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    resultado = true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+
+        }
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(Boleto boleto)
+        {
+            if (boleto == null)
+                return BadRequest();
+
+            if (ActualizarBoleto(boleto))
+                return Ok(boleto);
+            else
+                return InternalServerError();
+        }
+
+        private bool ActualizarBoleto(Boleto boleto)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"UPDATE BOLETO SET USU_CODIGO = @USU_CODIGO
+                                                                            ,VUE_ID = @VUE_ID
+                                                                            ,PAGO_ID = @PAGO_ID
+                                                                            ,BOL_FEC_IDA = @BOL_FEC_IDA
+                                                                            ,BOL_FEC_VUELTA = @BOL_FEC_VUELTA
+                                                                            ,BOL_PRECIO = @BOL_PRECIO
+                                                                            ,BOL_ASIENTO = @BOL_ASIENTO
+                                                                            ,BOL_TERMINAL = @BOL_TERMINAL
+                                                                      WHERE BOL_ID = @BOL_ID", sqlConnection);
+
+                sqlCommand.Parameters.AddWithValue("@BOL_ID", boleto.BOL_ID);
+                sqlCommand.Parameters.AddWithValue("@USU_CODIGO", boleto.USU_CODIGO);
+                sqlCommand.Parameters.AddWithValue("@VUE_ID", boleto.VUE_ID);
+                sqlCommand.Parameters.AddWithValue("@PAGO_ID", boleto.PAGO_ID);
+                sqlCommand.Parameters.AddWithValue("@BOL_FEC_IDA", boleto.BOL_FEC_IDA);
+                sqlCommand.Parameters.AddWithValue("@BOL_FEC_VUELTA", boleto.BOL_FEC_VUELTA);
+                sqlCommand.Parameters.AddWithValue("@BOL_PRECIO", boleto.BOL_PRECIO);
+                sqlCommand.Parameters.AddWithValue("@BOL_ASIENTO", boleto.BOL_ASIENTO);
+                sqlCommand.Parameters.AddWithValue("@BOL_TERMINAL", boleto.BOL_TERMINAL);
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    resultado = true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Eliminar(int id)
+        {
+            if (id < 1)
+                return BadRequest();
+
+            if (EliminarBoleto(id))
+                return Ok(id);
+            else
+                return InternalServerError();
+        }
+
+        private bool EliminarBoleto(int id)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"DELETE BOLETO
+                                                         WHERE BOL_ID = @BOL_ID", sqlConnection);
+
+                sqlCommand.Parameters.AddWithValue("@BOL_ID", id);
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    resultado = true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+
+        }
+    }
+}
---
+++ WebApiSegura/Controllers/VueloController.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSegura.Models;
+
+namespace WebApiSegura.Controllers
+{
+    //[Authorize]
+    [RoutePrefix("api/vuelo")]
+    public class VueloController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetId(int id)
+        {
+            Vuelo vuelo = new Vuelo();
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT VUE_ID, AV_ID, VUE_ORIGEN, VUE_DESTINO, VUE_CANT_PASAJEROS FROM VUELO
+                                                            WHERE VUE_ID = @VUE_ID", sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@VUE_ID", id);
+
+                    sqlConnection.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        vuelo.VUE_ID = sqlDataReader.GetInt32(0);
+                        vuelo.AV_ID = sqlDataReader.GetInt32(1);
+                        vuelo.VUE_ORIGEN = sqlDataReader.GetString(2);
+                        vuelo.VUE_DESTINO = sqlDataReader.GetString(3);
+                        vuelo.VUE_CANT_PASAJEROS = sqlDataReader.GetInt32(4);
+                    }
+
+                    sqlConnection.Close();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Ok(vuelo);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            List<Vuelo> vuelos = new List<Vuelo>();
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT VUE_ID, AV_ID, VUE_ORIGEN, VUE_DESTINO, VUE_CANT_PASAJEROS FROM VUELO", sqlConnection);
+
+                    sqlConnection.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Vuelo vuelo = new Vuelo()
+                        {
+                            VUE_ID = sqlDataReader.GetInt32(0),
+                            AV_ID = sqlDataReader.GetInt32(1),
+                            VUE_ORIGEN = sqlDataReader.GetString(2),
+                            VUE_DESTINO = sqlDataReader.GetString(3),
+                            VUE_CANT_PASAJEROS = sqlDataReader.GetInt32(4)
+                        };
+
+                        vuelos.Add(vuelo);
+                    }
+
+
+                    sqlConnection.Close();
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(vuelos);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Ingresar(Vuelo vuelo)
+        {
+            if (vuelo == null)
+                return BadRequest();
+
+            if (RegistrarVuelo(vuelo))
+                return Ok(vuelo);
+            else
+                return InternalServerError();
+        }
+
+        private bool RegistrarVuelo(Vuelo vuelo)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO VUELO (AV_ID, VUE_ORIGEN, VUE_DESTINO, VUE_CANT_PASAJEROS) 
+                                                            VALUES (@AV_ID, @VUE_ORIGEN, @VUE_DESTINO, @VUE_CANT_PASAJEROS)", sqlConnection);
+
+                sqlCommand.Parameters.AddWithValue("@VUE_ID", vuelo.VUE_ID);
+                sqlCommand.Parameters.AddWithValue("@AV_ID", vuelo.AV_ID);
+                sqlCommand.Parameters.AddWithValue("@VUE_ORIGEN", vuelo.VUE_ORIGEN);
+                sqlCommand.Parameters.AddWithValue("@VUE_DESTINO", vuelo.VUE_DESTINO);
+                sqlCommand.Parameters.AddWithValue("@VUE_CANT_PASAJEROS", vuelo.VUE_CANT_PASAJEROS);
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    resultado = true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+
+        }
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(Vuelo vuelo)
+        {
+            if (vuelo == null)
+                return BadRequest();
+
+            if (ActualizarVuelo(vuelo))
+                return Ok(vuelo);
+            else
+                return InternalServerError();
+        }
+
+        private bool ActualizarVuelo(Vuelo vuelo)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"UPDATE VUELO SET AV_ID = @AV_ID, 
+                                                                          VUE_ORIGEN = @VUE_ORIGEN, 
+                                                                          VUE_DESTINO = @VUE_DESTINO, 
+                                                                          VUE_CANT_PASAJEROS = @VUE_CANT_PASAJEROS
+                                                                      WHERE VUE_ID = @VUE_ID", sqlConnection);
+
+                sqlCommand.Parameters.AddWithValue("@VUE_ID", vuelo.VUE_ID);
+                sqlCommand.Parameters.AddWithValue("@AV_ID", vuelo.AV_ID);
+                sqlCommand.Parameters.AddWithValue("@VUE_ORIGEN", vuelo.VUE_ORIGEN);
+                sqlCommand.Parameters.AddWithValue("@VUE_DESTINO", vuelo.VUE_DESTINO);
+                sqlCommand.Parameters.AddWithValue("@VUE_CANT_PASAJEROS", vuelo.VUE_CANT_PASAJEROS);
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    resultado = true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Eliminar(int id)
+        {
+            if (id < 1)
+                return BadRequest();
+
+            if (EliminarVuelo(id))
+                return Ok(id);
+            else
+                return InternalServerError();
+        }
+
+        private bool EliminarVuelo(int id)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"DELETE VUELO
+                                                         WHERE VUE_ID = @VUE_ID", sqlConnection);
+
+                sqlCommand.Parameters.AddWithValue("@VUE_ID", id);
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    resultado = true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+
+        }
+    }
+}
